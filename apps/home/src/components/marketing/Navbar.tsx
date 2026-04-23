689b60993f60547a7617dde8514457f67162bf13
Lucas Pevidor <lucas.pevidor@joinplank.com>
2026-04-23 15:01:26 -0300

    chore: remove debug console.log from Navbar

--- apps/home/src/components/marketing/Navbar.tsx
+++ apps/home/src/components/marketing/Navbar.tsx
@@ -4,7 +4,6 @@ import { NavbarClient } from "./NavbarClient"
 export async function Navbar() {
   const supabase = await createClient()
   const { data: { user } } = await supabase.auth.getUser();
-  console.log(user);
   const displayName = user?.user_metadata?.full_name?.split(" ")[0] ?? user?.email ?? "User";
 
   return <NavbarClient user={user ? { displayName } : null} />
